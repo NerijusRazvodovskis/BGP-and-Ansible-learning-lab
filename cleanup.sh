176ff9dc66e48e8ec6bd5f40141614cd8bbf05eb
nerijusrazvodovskis <nerijus.razvodovskis@hostinger.com>
2024-11-03 17:45:46 +0200

    Init PR

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ssh-keygen -f "/home/nerijus/.ssh/known_hosts" -R "172.20.20.1"
+ssh-keygen -f "/home/nerijus/.ssh/known_hosts" -R "172.20.20.2"
+ssh-keygen -f "/home/nerijus/.ssh/known_hosts" -R "172.20.20.3"
+ssh-keygen -f "/home/nerijus/.ssh/known_hosts" -R "172.20.20.4"
+ssh-keygen -f "/home/nerijus/.ssh/known_hosts" -R "172.20.20.5"
+ssh-keygen -f "/home/nerijus/.ssh/known_hosts" -R "172.20.20.6"
+ssh-keygen -f "/home/nerijus/.ssh/known_hosts" -R "172.20.20.7"
+ssh-keygen -f "/home/nerijus/.ssh/known_hosts" -R "172.20.20.8"
+ssh-keygen -f "/home/nerijus/.ssh/known_hosts" -R "172.20.20.9"
